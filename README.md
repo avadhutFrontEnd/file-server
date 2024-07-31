c087ef1b51b6cdbb23628adef1e838bd1629e179
Avadhut Jagde <avadhutjagde555@gmail.com>
2024-07-31 20:48:24 +0530

    Remove deleted files from version control

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # file-server
 Next.js File Storage App with Role Based Authorization
+
+Step 1 : `cd file-drive`
+
+Step 2 : Run convex
+`npx convex dev`
+
+Step 3 : Run Next-js project 
+`npm run dev`
--- file-drive/src/middleware.ts
+++ file-drive/src/middleware.ts
@@ -7,7 +7,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
      '/(.*)',
-    '/dashboard(.*)',
+    // '/dashboard(.*)',
     // '/forum(.*)',
   ]);
   
